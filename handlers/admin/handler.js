7a45d80ef12a2445c567d5fd0ec19931fc0b002e
Andrianto Nee <andrianto.nee@gmail.com>
2016-06-06 12:10:07 +0700

    Penambahan Template Nunjucks pada app admin dan Pengubahan pada handler.js

--- handlers/admin/handler.js
+++ handlers/admin/handler.js
@@ -1,5 +1,13 @@
+var variabel_home = {
+	header : {
+		sidebar : {
+			"index.html" : "active"
+		}
+	}
+};
+
 home = function(req, res){
-	res.render('./admin/index.html', {});
+	res.render('./admin/home/home.html', {variabel_home : variabel_home});
 };
 
 handler = {
