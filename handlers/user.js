46a78d0ffdfbe4fbabdd2a783a153ee7d251c8b3
Andrianto Nee <andrianto.nee@gmail.com>
2016-06-24 17:10:01 +0700

    penambahan music_category pada user

--- handlers/user.js
+++ handlers/user.js
@@ -1,9 +1,46 @@
+var knex = require('knex')({
+		client: 'mysql',
+		connection: {
+			host     : '127.0.0.1',
+			user     : 'knexuser',
+			password : '12345',
+			database : 'knextest'
+		}
+	});
+
 home = function(req, res){
 	res.render('./user/pages/home/home.html', {});
 };
 
+music_category = function(req, res){
+	var music_category = {
+		tab_content_music_category : []
+	};
+
+	knex.select('category')
+		.table('music_category')
+		.orderBy('tanggal_waktu')
+		.then(function(rows){
+			music_category.tab_content_music_category = rows;
+			for (i = 0; i < music_category.tab_content_music_category.length; i++){
+				if (i === 0){
+					music_category.tab_content_music_category[i].class = "active";
+				}
+				else{
+					music_category.tab_content_music_category[i].class = "";
+				}
+			};
+			
+			res.render('./user/pages/music_category/music_category.html', {music_category : music_category});
+		})
+		.catch(function(err){
+			res.render('./user/pages/music_category/music_category.html', {music_category : music_category});
+		});
+};
+
 handler = {
 	home: home,
+	music_category : music_category
 };
 
 module.exports = handler;
